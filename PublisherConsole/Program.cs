526554da47fcece6ac12904b66b0c37be920f279
Ioan Scafa <85192263+therealisc@users.noreply.github.com>
2023-10-03 17:16:26 +0300

    First files

---
+++ PublisherConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using PublisherData;
+using PublisherDomain;
+
+using (PubContext context = new PubContext())
+{
+    context.Database.EnsureCreated();
+}
+
+GetAuthors();
+//AddAuthor();
+GetAuthors();
+
+void AddAuthor()
+{
+    var author = new Author { FirstName = "Stupid", LastName = "John" };
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+
+void GetAuthors()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
